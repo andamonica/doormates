fb950c6b43d4d311f537c4ea6c69743efc17d8ab
Qisheng Sun <dysonfabiensun@sjtu.edu.cn>
2026-03-08 17:33:12 +0800

    fix(core): fixed erroneous fetches from get_playerCharacter()

--- app/js/helper-functions/helper-functions.js
+++ app/js/helper-functions/helper-functions.js
@@ -19,7 +19,7 @@ function get_playerCharacter(idOverride) {
   //// Request character data via backend API ////
   ////////////////////////////////////////////////
 
-  fetch("api.character.get", {
+  fetch("/api.character.get", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ characterId: characterId }),
